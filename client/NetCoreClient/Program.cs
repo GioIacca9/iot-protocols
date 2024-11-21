be96ae90f914ae245d520e5113af484352adc555
GioIacca9 <giosue@iacca.ml>
2024-11-21 10:14:58 +0100

    Add MQTT support

--- client/NetCoreClient/Program.cs
+++ client/NetCoreClient/Program.cs
@@ -3,29 +3,24 @@ using NetCoreClient.Sensors;
 
 internal class Program
 {
-    public IProtocolInterface? protocol;
+    public MQTT protocol;
     public static void Main(string[] args)
     {
-        Program main = new ();
+        Program main = new();
         main.Run();
     }
 
     private void Run()
     {
-        protocol = new Http("https://iot.scuola.iacca.ml/temperature");
-        //protocol = new Http("https://dioenac.requestcatcher.com/");
+        protocol = new MQTT("iot.scuola.iacca.ml", 1883, "mqtt-01", "", "");
+        protocol.Connect();
         Dht11 sensor = new();
-        List<ISensorInterface> sensors = [sensor];
-        sensor.SerialDataReceived += AirTemperatureChanged;
 
-        while (true) { }
-    }
-
-    private void AirTemperatureChanged(object? sender, PropertyChangedEventArgs e)
-    {
-        Dht11 dht11 = (Dht11)sender;
-        string sensorValue = dht11.ToJson();
-        protocol?.Send(sensorValue);
-        Console.WriteLine("Data sent: " + sensorValue);
+        while (true)
+        {
+            sensor.ReadValues();
+            protocol.Send("iot/casetta/data", sensor.ToJson());
+            Thread.Sleep(1000);
+        }
     }
 }
