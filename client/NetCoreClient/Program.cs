e21ea7245f2a2e76018625dee050e10c1c7ed428
GioIacca9 <giosue@iacca.ml>
2024-11-21 12:37:25 +0100

    Add MQTT commands support

--- client/NetCoreClient/Program.cs
+++ client/NetCoreClient/Program.cs
@@ -1,26 +1,45 @@
-﻿using NetCoreClient.Protocols;
+﻿using MQTTnet.Client;
+using NetCoreClient.Protocols;
 using NetCoreClient.Sensors;
+using System.Text;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 internal class Program
 {
-    public MQTT protocol;
-    public static void Main(string[] args)
+    private static void Main()
     {
-        Program main = new();
-        main.Run();
+        ClientServices cloudServices = new();
+        cloudServices.Start();
+
+        while (true) { }
+    }
+}
+
+public class ClientServices()
+{
+    public MqttProvider? MqttProvider { get; private set; }
+    public async Task Start()
+    {
+        MqttProvider = new("iot.scuola.iacca.ml", 1883, "mqtt-01");
+        Dht11 sensor = new();
+        if (MqttProvider != null && await MqttProvider.Connect())
+        {
+            if (!await MqttProvider.SubscribeTopic("iot/0001/commands/#"))
+                Console.WriteLine("Errore sottoscrivendo al topic.");
+            MqttProvider.MessageReceived += MessageReceived;
+            u
+            while (true)
+            {
+                sensor.ReadValues();
+                await MqttProvider.Publish("iot/0001/data", sensor.ToJson());
+                Thread.Sleep(1000);
+            }
+        }
     }
 
-    private void Run()
+    private void MessageReceived(object? sender, EventArgs args)
     {
-        protocol = new MQTT("iot.scuola.iacca.ml", 1883, "mqtt-01", "", "");
-        protocol.Connect();
-        Dht11 sensor = new();
-
-        while (true)
-        {
-            sensor.ReadValues();
-            protocol.Send("iot/casetta/data", sensor.ToJson());
-            Thread.Sleep(1000);
-        }
+        var mqttArgs = (MqttApplicationMessageReceivedEventArgs)args;
+        Console.WriteLine("Command received: " + mqttArgs.ApplicationMessage.Topic  + " " + Encoding.UTF8.GetString(mqttArgs.ApplicationMessage.PayloadSegment));
     }
 }
